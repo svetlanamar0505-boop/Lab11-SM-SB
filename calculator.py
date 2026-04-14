dc3ed80da5e05c6d9d4110fa074a4f61bd7341c4
Svetlana Martin <svetlanasmartin@icloud.com>
2026-04-13 21:46:44 -0400

    modifies calculator p1

--- calculator.py
+++ calculator.py
@@ -33,5 +33,36 @@ def exp(a, b):
     return a ** b
 
 
+import math
+
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return b / a
+
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Math domain error: base must be > 0 and != 1, and argument must be > 0.")
+    return math.log(b, a)
+
+
+def exp(a, b):
+    return a ** b
+
+
 
 
